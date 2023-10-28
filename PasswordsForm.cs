528f311b558955e3a5ca0cec75f74ea60c09887f
zipianli818 <zipian.li818@gmail.com>
2023-10-28 18:14:13 +1100

    Add PasswordsForm.cs

---
+++ PasswordsForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Password_Manager
+{
+    public partial class PasswordsForm : Form
+    {
+        public PasswordsForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
