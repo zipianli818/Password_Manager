7c762a1c602fa2d59c617ff9a6f3a919a291b382
zipianli818 <100552310+zipianli818@users.noreply.github.com>
2023-10-29 17:48:56 +1100

    Update PasswordsForm.cs

--- PasswordsForm.cs
+++ PasswordsForm.cs
@@ -1,7 +1,10 @@
-﻿using System;
+using Password_Manager.Controls;
+using Password_Manager.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +15,75 @@ namespace Password_Manager
 {
     public partial class PasswordsForm : Form
     {
+        public PasswordPropertyTextAttribute CurrentPassword { get; set; }
+
+        private DataContext dataContext = new DataContext();
         public PasswordsForm()
         {
             InitializeComponent();
+
+        }
+
+        private void PasswordsForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String passwordID = PasswordId.Text;
+            String passwordName = PasswordName.Text;
+            String password = Password.Text;
+            
+            MessageBox.Show("Password ID: " + PasswordId + " " +
+                            "Password Name: " + PasswordName + " " +
+                            "Password: " + Password + " " +
+                            "have been saved!");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
